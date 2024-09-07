8f5cd1224461d4b0813819e195b163ebbe4fbab3
Santiago <glaze@outlook.es>
2024-09-07 14:13:33 -0500

    refactor: migrate CropZoom to the new pinch implementation

--- src/components/crop/types.ts
+++ src/components/crop/types.ts
@@ -1,17 +1,11 @@
 import type React from 'react';
 import type {
   CommonResumableProps,
-  CommonZoomProps,
   PanGestureCallbacks,
   PinchGestureCallbacks,
   SizeVector,
   TapGestureCallbacks,
 } from '../../commons/types';
-
-export enum CropMode {
-  MANAGED,
-  OVERLAY,
-}
 
 export type CropContextResult = {
   crop: {
@@ -39,17 +33,17 @@ export type CropZoomState<T> = {
   rotateY: T;
 };
 
-export type CropZoomAssignableState = Omit<
+export type CropAssignableState = Omit<
   CropZoomState<number>,
   'width' | 'height'
 >;
 
-export type RotateTransitionCallback = (
+export type FlipCallback = (
   animate?: boolean,
   cb?: (value: number) => void
 ) => void;
 
-export type DirectionRotationCallback = (
+export type RotationCallback = (
   animate?: boolean,
   clockwise?: boolean,
   cb?: (value: number) => void
@@ -57,10 +51,10 @@ export type DirectionRotationCallback = (
 
 export type CropZoomType = {
   requestState: () => CropZoomState<number>;
-  assignState: (state: CropZoomAssignableState, animate?: boolean) => void;
-  rotate: DirectionRotationCallback;
-  flipHorizontal: RotateTransitionCallback;
-  flipVertical: RotateTransitionCallback;
+  assignState: (state: CropAssignableState, animate?: boolean) => void;
+  rotate: RotationCallback;
+  flipHorizontal: FlipCallback;
+  flipVertical: FlipCallback;
   reset: (animate?: boolean) => void;
   crop: (fixedWidth?: number) => CropContextResult;
 };
@@ -70,13 +64,10 @@ export type CropGestureEventCallBack = (event: CropZoomState<number>) => void;
 export type CropZoomProps = React.PropsWithChildren<{
   cropSize: SizeVector<number>;
   resolution: SizeVector<number>;
-  debug?: boolean;
-  mode?: CropMode;
   onUpdate?: CropGestureEventCallBack;
   OverlayComponent?: () => React.ReactElement<any>;
 }> &
   PanGestureCallbacks &
   PinchGestureCallbacks &
   Omit<TapGestureCallbacks, 'onDoubleTap'> &
-  Omit<CommonZoomProps, 'hitSlop' | 'timingConfig'> &
   CommonResumableProps;
