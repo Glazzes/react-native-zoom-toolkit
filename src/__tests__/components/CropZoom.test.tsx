a042d911ab8b4c144569c861cb642a03a77a98d5
Santiago <glaze@outlook.es>
2024-09-07 15:03:29 -0500

    test: refactor CropZoom tests

--- src/__tests__/components/CropZoom.test.tsx
+++ src/__tests__/components/CropZoom.test.tsx
@@ -3,13 +3,12 @@ import { View } from 'react-native';
 import { render } from '@testing-library/react-native';
 
 import CropZoom from '../../components/crop/CropZoom';
-import { CropMode } from '../../components/crop/types';
 import type { SizeVector } from '../../commons/types';
 
 const cropSize: SizeVector<number> = { width: 100, height: 100 };
 const resolution: SizeVector<number> = { width: 100, height: 200 };
 
-describe('CropZoom', () => {
+describe('CropZoom Property Tests', () => {
   afterEach(() => {
     jest.restoreAllMocks();
   });
@@ -24,56 +23,9 @@ describe('CropZoom', () => {
     ).toBeDefined();
   });
 
-  test('should throw error in managed mode when no child is passed', () => {
-    const message = `CropZoom expected one child but received 0 children`;
-
-    expect(() =>
-      render(
-        <CropZoom
-          mode={CropMode.MANAGED}
-          cropSize={cropSize}
-          resolution={resolution}
-        />
-      )
-    ).toThrow(message);
-  });
-
-  test('should render properly in overlay mode when no children are passed', () => {
-    expect(
-      render(
-        <CropZoom
-          cropSize={cropSize}
-          resolution={resolution}
-          mode={CropMode.OVERLAY}
-        />
-      )
-    ).toBeDefined();
-  });
-
-  test('should throw error in overlay mode when children are passed', () => {
-    const message = `CropZoom expected no children but received 1 children`;
-
-    expect(() =>
-      render(
-        <CropZoom
-          cropSize={cropSize}
-          resolution={resolution}
-          mode={CropMode.OVERLAY}
-        >
-          <View />
-        </CropZoom>
-      )
-    ).toThrow(message);
-  });
-
   test('should throw error if minScale property is less than one', () => {
     const cropzoom = (
-      <CropZoom
-        cropSize={cropSize}
-        resolution={resolution}
-        mode={CropMode.OVERLAY}
-        minScale={-1}
-      />
+      <CropZoom cropSize={cropSize} resolution={resolution} minScale={-1} />
     );
 
     expect(() => render(cropzoom)).toThrow();
@@ -83,12 +35,7 @@ describe('CropZoom', () => {
     const random = Math.random();
 
     const cropzoom = (
-      <CropZoom
-        cropSize={cropSize}
-        resolution={resolution}
-        mode={CropMode.OVERLAY}
-        maxScale={random}
-      />
+      <CropZoom cropSize={cropSize} resolution={resolution} maxScale={random} />
     );
 
     expect(() => render(cropzoom)).toThrow();
@@ -99,7 +46,6 @@ describe('CropZoom', () => {
       <CropZoom
         cropSize={cropSize}
         resolution={resolution}
-        mode={CropMode.OVERLAY}
         minScale={10}
         maxScale={5}
       />
