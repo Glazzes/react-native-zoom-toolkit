df7b6acd3a3b92ce459498b29d19e6910896276d
Santiago <glaze@outlook.es>
2024-09-25 17:05:50 -0500

    test!: add gesture tests and rename component tests to validation

---
+++ src/__tests__/validation/ResumableZoomValidation.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View } from 'react-native';
+import { render } from '@testing-library/react-native';
+import ResumableZoom from '../../components/resumable/ResumableZoom';
+
+describe('ResumableZoom Validation Tests', () => {
+  it('should throw error if not child is passed', () => {
+    expect(() => {
+      return render(<ResumableZoom>{undefined}</ResumableZoom>);
+    }).toThrow();
+  });
+
+  it('should throw error when minScale is bigger than maxScale', () => {
+    expect(() => {
+      return render(
+        <ResumableZoom minScale={10} maxScale={5}>
+          <View style={{ width: 200, height: 200 }} />
+        </ResumableZoom>
+      );
+    }).toThrow();
+  });
+
+  it('should throw error when minScale is lesser than one', () => {
+    expect(() => {
+      return render(
+        <ResumableZoom minScale={0.5}>
+          <View style={{ width: 200, height: 200 }} />
+        </ResumableZoom>
+      );
+    }).toThrow();
+  });
+
+  it('should throw error when maxScale is lesser than one', () => {
+    expect(() => {
+      return render(
+        <ResumableZoom maxScale={0.5}>
+          <View style={{ width: 200, height: 200 }} />
+        </ResumableZoom>
+      );
+    }).toThrow();
+  });
+});
