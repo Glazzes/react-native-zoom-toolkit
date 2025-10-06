697d062e4a51255777139df0ff7f881a265e1ef7
glazzes <santiagop1@protonmail.com>
2025-10-05 19:57:39 -0500

    build(deps): update project to Expo 54

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    '@babel/plugin-proposal-export-namespace-from',
-    'react-native-reanimated/plugin',
-  ],
 };
--- example/babel.config.js
+++ example/babel.config.js
@@ -17,8 +17,6 @@ module.exports = function (api) {
           },
         },
       ],
-      '@babel/plugin-proposal-export-namespace-from',
-      'react-native-reanimated/plugin',
     ],
   };
 };
